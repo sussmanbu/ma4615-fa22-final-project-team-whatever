f030958b71999051ebccbaed38a3a0ec16d2623d
Luke DiIorio <ldiiorio@bu.edu>
2022-10-24 09:31:30 -0400

    test

--- static/load_and_clean_data.R
+++ static/load_and_clean_data.R
@@ -1,10 +1,11 @@
 library(tidyverse)
-
 nutrition_data <- readxl::read_xlsx("dataset/Nutrition-Dataset.xlsx")
-
 ## CLEAN the data
-loan_data_clean <- loan_data
+loan_data_clean <- nutrition_data
 
-write_csv(loan_data_clean, file = here::here("dataset", "loan_refusal_clean.csv"))
+write_csv(loan_data_clean, file = here::here("dataset", "Nutrition-Dataset.xlsx"))
 
-save(loan_data_clean, file = here::here("dataset/loan_refusal.RData"))
+save(loan_data_clean, file = here::here("dataset/Nutrition-Dataset.xlsx"))
+
+data <- nutrition_data[-(218:246),]
+
