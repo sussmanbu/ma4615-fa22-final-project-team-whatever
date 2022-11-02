46db43b529df5db2c9c94e974f748475b64907bd
Anamaria Pananas <apananas@bu.edu>
2022-11-02 18:07:32 -0400

    ap

--- static/load_and_clean_data.R
+++ static/load_and_clean_data.R
@@ -1,6 +1,8 @@
 library(tidyverse)
 nutrition_data <- readxl::read_xlsx("dataset/Nutrition-Dataset.xlsx")
 ## CLEAN the data
+
+#gets rid of NA columns 
 loan_data_clean <- nutrition_data
 
 write_csv(loan_data_clean, file = here::here("dataset", "Nutrition-Dataset.xlsx"))
