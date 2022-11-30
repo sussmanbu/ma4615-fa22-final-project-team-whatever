87c55d6703c1cf191190f9ab9828e40bfdf380df
Francesco Benatti <fbenatti@bu.edu>
2022-11-30 11:18:29 -0500

    Pop_Size attempt

--- static/load_and_clean_data.R
+++ static/load_and_clean_data.R
@@ -261,3 +261,29 @@ income_mortality$`Year` <- round(as.numeric(income_mortality$`Year`), 2)
 income_mortality$`Income` <- round(as.numeric(income_mortality$`Income`), 2)
 
 save(income_mortality, file = "dataset/income_mortality.RData")
+
+# Including countries' population size
+
+suppressPackageStartupMessages(library(tidyverse))
+pop_size <- read_csv("dataset/population_total.csv")
+pop_size <- pop_size[c(1,153:223)]
+
+
+# Function to detect and remove M from dataset
+
+#how to include both "k" and "M" in the function?
+fun2 <- 
+  function(x){
+    M <- str_detect(x, "M") 
+    for(i in 1:length(M)){
+      if(M[i]){x[i] <- as.numeric(substr(x[i],1,nchar(x[i])-1))*1000000}
+      if(!M[i]){x[i] <- as.numeric(x[i])}
+    }
+    return(x)
+  }
+
+
+#Joining Datasets
+pop_size <- cbind(pop_size$country,apply(pop_size[,2:72],2,fun2))
+
+pop_size <-as.data.frame(pop_size)%>%pivot_longer(-V1)
